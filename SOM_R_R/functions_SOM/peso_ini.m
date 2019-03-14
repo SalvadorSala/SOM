71c392f077db1641df4422ca9b0a5839dcf82785
SSalaPla <ssalapla@gmail.com>
2019-03-14 15:10:29 +0100

    Remove duplicated directory

--- SOM_R_R/functions_SOM/peso_ini.m
+++
@@ -1,9 +0,0 @@
-function[pesoi]=peso_ini(xmat_pos,ymat_pos,pesos)
-
-for i=1:length(pesos)
-
- pesoi(i,:)=[sum((xmat_pos(:)'.*reshape(pesos(:,:,i),[1,length(pesos)])))./sum(reshape(pesos(:,:,i),[1,length(pesos)])),...
-  sum((ymat_pos(:)'.*reshape(pesos(:,:,i),[1,length(pesos)])))./sum(reshape(pesos(:,:,i),[1,length(pesos)]))];
-
-end
-end
